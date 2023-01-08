7c5c1c5e94cefebef15dd51adada834e13ccd0b5
aydın diler <51709512+aydin0101@users.noreply.github.com>
2023-01-08 13:24:00 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+   int luckyNumbers [] ={6,9,12,17,37,60};
+
+   int i;
+
+    for (i=0;i<6;i++){
+        printf("%d\n",luckyNumbers[i]);
+    }
+    printf("Cikmak icin bir tusa basin...\n");
+    getchar();
+
+    return 0;
+}
